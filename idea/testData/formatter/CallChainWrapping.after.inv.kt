efbab98acbad7dc4f95b5f86a7a07e45c3285470
Dmitry Jemerov <yole@jetbrains.com>
2017-11-30 16:17:09 +0100

    Initial support for chained call wrapping options
     #KT-21529 Fixed

---
+++ idea/testData/formatter/CallChainWrapping.after.inv.kt
@@ -0,0 +1,9 @@
+val x = foo
+        .bar()
+        .baz()
+        .quux()
+
+val y = xyzzy(foo.bar().baz().quux())
+
+// SET_INT: METHOD_CALL_CHAIN_WRAP = 2
+// SET_FALSE: WRAP_FIRST_METHOD_IN_CALL_CHAIN
